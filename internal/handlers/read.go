f615f296a848ca15da3eb5e6c8bda88d4c4fda29
Limezy <arnaudetpia@de-ruffray.fr>
2024-11-30 12:01:38 +0700

    Update read.go

--- internal/handlers/read.go
+++ internal/handlers/read.go
@@ -2,7 +2,20 @@ package handlers
 
 import (
 	"fmt"
+	"time"
 )
+
+// HandleRegisterAction listens for detected card UID and simulates the "Register" action.
+func HandleRegisterAction(cardDetectedChan <-chan string) {
+	// Simulate waiting for a card to be detected
+	for {
+		select {
+		case uid := <-cardDetectedChan:
+			// Simulate registering the card by printing a message to the console
+			fmt.Printf("Registering card %s\n", uid)
+		}
+	}
+}
 
 // HandleReadAction listens for detected card UID and simulates the "Play" action.
 func HandleReadAction(cardDetectedChan <-chan string, modeSwitch <-chan bool) {
